b6f37fe856cd9124e49eed8fac3b5cb33570bba1
unknown <juratbekxudayberganov9@gmail.com>
2023-03-24 21:19:19 +0500

    changes

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.overallBand = void 0;
-const overallBand = (listening, reading, writing, speaking) => {
+var overallBand = function (listening, reading, writing, speaking) {
     // overall calculating
-    let calc = (listening + reading + writing + speaking) / 4;
+    var calc = (listening + reading + writing + speaking) / 4;
     // getting residual
-    let residual = calc - Math.trunc(calc);
+    var residual = calc - Math.trunc(calc);
     //conditional checking
     if (residual === 0) {
         return calc;
     }
     else if (residual >= 0.25 && residual < 0.75) {
-        return `${Math.floor(calc)}.5`;
+        return "".concat(Math.floor(calc), ".5");
     }
     else if (residual <= 0.25) {
         return Math.floor(calc);
